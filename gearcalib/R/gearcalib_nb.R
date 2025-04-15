e3aebce976050f9a5c97a6ed8e57656b1a948bee
Sean Rohan <sean.rohan@noaa.gov>
2025-04-15 12:55:15 -0700

    Fix ZIP

--- gearcalib/R/gearcalib_nb.R
+++ gearcalib/R/gearcalib_nb.R
@@ -19,123 +19,124 @@ gearcalibFitNB <-
            logtheta=NA,
            logitpi=NA,
            model = "poisson") {
-  check.gearcalib.data(d)
-  nsize <- ncol(d$N)
-  ngroup <- nlevels(d$group)
-  nhaul <- nrow(d$N)
-  ngear <- nlevels(d$Gear)
-  
-  ## Set default RW order
-  rw_order <- c(1,1)
-  
-  data <- list(
-    N=d$N,
-    SweptArea=d$SweptArea,
-    group=d$group,
-    Gear=d$Gear,
-    huge=10,
-    tiny=0.01,
-    rw_order=rw_order
-  )
-  
-  data$model_type <- 
-    switch(
-      model,
-      "poisson" = 1,
-      "nb" = 2,
-      "zip" = 3
-    )
-  
-  parameters <- 
-        list(
-          logspectrum=matrix(0,ngroup,nsize),
-          nugget=matrix(0,nhaul,nsize),
-          residual=matrix(0,nhaul,nsize),
-          loggear=numeric(nsize),
-          logsd=-1,
-          phi=0.9,
-          logsdnug=-1,
-          logsdres=-1,
-          logsdGearRW=-1,
-          logalpha = 0,
-          logtheta = 0)
-
-  
-  random <- c("logspectrum","residual","loggear","nugget")
-  
-  map <- list()
-  
-  ## If any parameters have been specified in the call, do not estimate those
-  ## parameters but fix them to the specified value
-  setparameter <- function(name) {
-    var <- get(name)
-    if(!is.na(var))
-    {
-      map[[name]] <<- factor(NA)
-      parameters[[name]] <<- var
-    }
-  }
-  parameternames <- c("logsd","phi","logsdnug","logsdres","logsdGearRW","logalpha","logtheta","logitpi")
-  sapply(parameternames, setparameter)
-  
-  obj <- MakeADFun(
-    data = data,
-    parameters = parameters,
-    map = map, 
-    random = random,
-    DLL="gearcalib"
-  )
-  
-  
-  lower <- 0*obj$par-Inf
-  upper <- 0*obj$par+Inf
-  if(any("phi" == names(obj$par)))
-  {
-    lower["phi"] <- 0
-    upper["phi"] <- 0.99
-  }
-  
-  
-  obj$env$tracepar <- TRUE
-  
-  system.time( opt <- nlminb(obj$par,obj$fn,obj$gr,lower=lower,upper=upper) )
-  Pvalue <- NA
-  
-  if(fit0)
-  {
-    parameters0 <- parameters
-    parameters0$logsdGearRW <- -10
+    check.gearcalib.data(d)
+    nsize <- ncol(d$N)
+    ngroup <- nlevels(d$group)
+    nhaul <- nrow(d$N)
+    ngear <- nlevels(d$Gear)
     
-    obj0 <- MakeADFun(
+    ## Set default RW order
+    rw_order <- c(1,1)
+    
+    data <- list(
+      N=d$N,
+      SweptArea=d$SweptArea,
+      group=d$group,
+      Gear=d$Gear,
+      huge=10,
+      tiny=0.01,
+      rw_order=rw_order
+    )
+    
+    data$model_type <- 
+      switch(
+        model,
+        "poisson" = 1,
+        "nb" = 2,
+        "zip" = 3
+      )
+    
+    parameters <- 
+      list(
+        logspectrum=matrix(0,ngroup,nsize),
+        nugget=matrix(0,nhaul,nsize),
+        residual=matrix(0,nhaul,nsize),
+        loggear=numeric(nsize),
+        logsd=-1,
+        phi=0.9,
+        logsdnug=-1,
+        logsdres=-1,
+        logsdGearRW=-1,
+        logalpha = 0,
+        logtheta = 0,
+        logitpi=0)
+    
+    
+    random <- c("logspectrum","residual","loggear","nugget")
+    
+    map <- list()
+    
+    ## If any parameters have been specified in the call, do not estimate those
+    ## parameters but fix them to the specified value
+    setparameter <- function(name) {
+      var <- get(name)
+      if(!is.na(var))
+      {
+        map[[name]] <<- factor(NA)
+        parameters[[name]] <<- var
+      }
+    }
+    parameternames <- c("logsd","phi","logsdnug","logsdres","logsdGearRW","logalpha","logtheta","logitpi")
+    sapply(parameternames, setparameter)
+    
+    obj <- MakeADFun(
       data = data,
-      parameters = parameters0,
-      map = c(map,list(logsdGearRW=factor(NA))),
+      parameters = parameters,
+      map = map, 
       random = random,
       DLL="gearcalib"
     )
     
-    obj0$env$tracepar <- TRUE
     
-    system.time( opt0 <- nlminb(obj0$par,obj0$fn,obj0$gr,lower=lower,upper=upper) )
+    lower <- 0*obj$par-Inf
+    upper <- 0*obj$par+Inf
+    if(any("phi" == names(obj$par)))
+    {
+      lower["phi"] <- 0
+      upper["phi"] <- 0.99
+    }
     
-    Pvalue <- pchisq(2*(opt0$objective-opt$objective),df=1)
     
-    print(paste("Chisq-test of no size structure in gear effect:",Pvalue))
+    obj$env$tracepar <- TRUE
     
-    rm(obj0)
+    system.time( opt <- nlminb(obj$par,obj$fn,obj$gr,lower=lower,upper=upper) )
+    Pvalue <- NA
+    
+    if(fit0)
+    {
+      parameters0 <- parameters
+      parameters0$logsdGearRW <- -10
+      
+      obj0 <- MakeADFun(
+        data = data,
+        parameters = parameters0,
+        map = c(map,list(logsdGearRW=factor(NA))),
+        random = random,
+        DLL="gearcalib"
+      )
+      
+      obj0$env$tracepar <- TRUE
+      
+      system.time( opt0 <- nlminb(obj0$par,obj0$fn,obj0$gr,lower=lower,upper=upper) )
+      
+      Pvalue <- pchisq(2*(opt0$objective-opt$objective),df=1)
+      
+      print(paste("Chisq-test of no size structure in gear effect:",Pvalue))
+      
+      rm(obj0)
+    }
+    
+    rep <- sdreport(obj)
+    repsum <- summary(rep,"random")
+    s <- repsum[grep("gear",rownames(repsum)),]
+    est <- 2*s[,1]
+    sd <- 2*s[,2]
+    
+    ret <- list(d=d,
+                Pvalue=Pvalue,rep=rep,opt=opt,obj=obj,est=est,sd=sd)
+    
+    class(ret)<-"gearcalibFit" 
+    
+    return(ret)
+    
   }
-  
-  rep <- sdreport(obj)
-  repsum <- summary(rep,"random")
-  s <- repsum[grep("gear",rownames(repsum)),]
-  est <- 2*s[,1]
-  sd <- 2*s[,2]
-  
-  ret <- list(d=d,
-              Pvalue=Pvalue,rep=rep,opt=opt,obj=obj,est=est,sd=sd)
-  
-  class(ret)<-"gearcalibFit" 
-  
-  return(ret)
-  
-}
